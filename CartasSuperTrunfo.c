c891ea07a8e34c9321c1a48789d1eb2e0c627b14
Linus <77833541+LinusCoelho@users.noreply.github.com>
2026-06-24 21:18:07 -0300

    Update CartasSuperTrunfo.c
    O nível novato foi concluído!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,49 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char Estado; 
+  char Cidade [10];
+  int Populacao;
+  float Pib;
+  int Pontos_Turisticos;
+  float Area;
+  char Codigo_Carta [5];
+
 
   // Área para entrada de dados
+  printf("Digite a letra do estado (A a H):\n");
+  scanf(" %c", &Estado);
+
+  printf("Digite uma cidade:\n");
+  scanf("%s", Cidade);
+
+  printf("Digite o código da carta:\n");
+  scanf("%s", Codigo_Carta);
+
+  printf("Qual o tamanho de sua população?\n");
+  scanf("%d", &Populacao);
+
+  printf("E qual o valor do seu Pib?\n");
+  scanf("%f", &Pib);
+
+  printf("Quantos pontos turisticos tem nessa cidade?\n");
+  scanf("%d", &Pontos_Turisticos);
+
+  printf("Qual o tamanho da Área dessa cidade\n");
+  scanf("%f", &Area);
 
   // Área para exibição dos dados da cidade
+  printf("\n=========================\n");
+  printf("     Carta Cadastrada    \n");
+  printf("=========================\n");
+  printf("Estado: %c\n", Estado);
+  printf("Cidade: %s\n", Cidade);
+  printf("Código da Carta: %s\n", Codigo_Carta);
+  printf("População: %d\n", Populacao);
+  printf("Pib: %.2f M\n", Pib);
+  printf("Área: %.2f km²\n", Area);
+  printf("Pontos Turisticos: %d\n", Pontos_Turisticos);
+  printf("=========================\n");
 
 return 0;
 } 
